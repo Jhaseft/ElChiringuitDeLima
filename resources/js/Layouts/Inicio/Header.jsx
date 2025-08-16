8b1e17e7553d2624e2c4e59670a38f3494d7c88a
Jhaseft <jhasesaat@gmail.com>
2025-08-15 21:43:18 -0400

    cambios finales

--- resources/js/Layouts/Inicio/Header.jsx
+++ resources/js/Layouts/Inicio/Header.jsx
@@ -68,22 +68,6 @@ export default function Header() {
         setMenuOpen={setMenuOpen}
       />
 
-      {/* Animación */}
-      <style jsx>{`
-        @keyframes fade-slide {
-          0% {
-            opacity: 0;
-            transform: translateY(-10px);
-          }
-          100% {
-            opacity: 1;
-            transform: translateY(0);
-          }
-        }
-        .animate-fade-slide {
-          animation: fade-slide 0.25s ease-out;
-        }
-      `}</style>
     </header>
   );
 }
