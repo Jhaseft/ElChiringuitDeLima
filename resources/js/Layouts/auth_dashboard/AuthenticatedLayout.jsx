34d8ee5d11c6f2f7c642e5aaa164c9f33737295a
Jhaseft <jhasesaat@gmail.com>
2025-08-22 22:34:16 -0400

    vamos

--- resources/js/Layouts/auth_dashboard/AuthenticatedLayout.jsx
+++ resources/js/Layouts/auth_dashboard/AuthenticatedLayout.jsx
@@ -1,7 +1,7 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import Dropdown from '@/Components/Dropdown';
-import NavLink from '@/Components/NavLink';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import ApplicationLogo from '@/Components/auht/ApplicationLogo';
+import Dropdown from '@/Components/auht/Components/Dropdown';
+import NavLink from '@/Components/auht/Components/NavLink';
+import ResponsiveNavLink from '@/Components/auht/Components/ResponsiveNavLink';
 import { Link, usePage } from '@inertiajs/react';
 import { useState } from 'react';
 
