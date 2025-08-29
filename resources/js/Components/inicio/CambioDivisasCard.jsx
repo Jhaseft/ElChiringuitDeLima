8ffc28618175f7585b6e893028370a15a3c7fe3a
Jhaseft <jhasesaat@gmail.com>
2025-08-29 15:13:04 -0400

    va

--- resources/js/Components/inicio/CambioDivisasCard.jsx
+++ resources/js/Components/inicio/CambioDivisasCard.jsx
@@ -172,22 +172,22 @@ export default function CambioDivisasCard({ tasas, bancos }) {
             </Link>
           </div>
         )}
- <p className="text-green-800 text-lg mt-4 text-center">
-            Para personas que quieran enviar dinero a terceros, esta operación debe ser realizada mediante la atención de un asesor.
-          </p>
+        <p className="text-green-800 text-lg mt-4 text-center">
+          Para personas que quieran enviar dinero a terceros, esta operación debe ser realizada mediante la atención de un asesor.
+        </p>
 
-          <div className="flex justify-center mt-2">
-            <svg
-              className="w-6 h-6 text-green-800  animate-pulse rotate-0  lg:-rotate-90"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"></path>
-            </svg>
-          </div>
+        <div className="flex justify-center mt-2">
+          <svg
+            className="w-6 h-6 text-green-800  animate-pulse rotate-0  lg:-rotate-90"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"></path>
+          </svg>
+        </div>
       </div>
 
 
