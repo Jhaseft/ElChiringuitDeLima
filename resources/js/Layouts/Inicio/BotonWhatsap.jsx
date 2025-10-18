118240e20281e11ea439b9d6ee4d44a554e5e26b
Jhaseft <jhasesaat@gmail.com>
2025-10-18 11:05:04 -0400

    fix: Update WhatsApp link and add video source to AppNative page

--- resources/js/Layouts/Inicio/BotonWhatsap.jsx
+++ resources/js/Layouts/Inicio/BotonWhatsap.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function BotonWhatsApp() {
   return (
     <a
-      href="https://wa.me/59165359695?text=Hola%2C%20quiero%20más%20información"
+      href="https://wa.me/59177958109?text=Hola%2C%20quiero%20más%20información"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white rounded-full p-4 shadow-lg flex items-center space-x-2 hover:bg-green-600 transition transform hover:scale-105"
--- resources/js/Pages/AppNative.jsx
+++ resources/js/Pages/AppNative.jsx
@@ -11,7 +11,7 @@ return (
             <RectanguloLayout background="#ffffff">
                 <LogoConVideo
                     logoSrc="https://res.cloudinary.com/dnbklbswg/image/upload/v1756305635/logo_n6nqqr.jpg"
-                    videoSrc=""
+                    videoSrc="https://res.cloudinary.com/dqemzxnqj/video/upload/v1760799045/Video_de_WhatsApp_2025-10-18_a_las_10.50.10_24e89e79_dsnmkj.mp4"
                 >
                    
                     <p className="text-2xl text-black font-extrabold sm:text-xl md:text-5xl font-montserrat  leading-snug sm:leading-tight max-w-[600px] mx-auto">
