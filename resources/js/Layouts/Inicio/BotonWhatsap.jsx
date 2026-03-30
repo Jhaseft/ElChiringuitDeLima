e4dca6495cd5d934ba9f9b5d94d9e93ef388a9b9
Jhaseft <jhasesaat@gmail.com>
2026-03-30 12:20:36 -0400

    cambio boton de whatsap

--- resources/js/Layouts/Inicio/BotonWhatsap.jsx
+++ resources/js/Layouts/Inicio/BotonWhatsap.jsx
@@ -7,9 +7,9 @@ export default function BotonWhatsApp() {
       href="https://wa.me/59177958109?text=Hola%2C%20quiero%20más%20información"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-6 right-6 bg-green-500 text-white rounded-full p-4 shadow-lg flex items-center space-x-2 hover:bg-green-600 transition transform hover:scale-105"
+      className="fixed bottom-6 right-6 bg-green-500 text-white rounded-full p-3 shadow-lg flex items-center space-x-2 hover:bg-green-600 transition transform hover:scale-105"
     >
-      <FaWhatsapp size={24} />
+      <FaWhatsapp size={32} />
       <span className="hidden md:inline font-medium">¡Escríbenos!</span>
     </a>
   );
