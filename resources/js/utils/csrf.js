4b281358c7f0b15346ec2cf4cf4f7722c06560c3
Jhaseft <jhasesaat@gmail.com>
2026-05-09 19:58:53 -0400

    cambios en crf

--- resources/js/utils/csrf.js
+++ resources/js/utils/csrf.js
@@ -1,5 +1,3 @@
 export function getCsrfToken() {
-    const match = document.cookie.match(/(?:^|;\s*)XSRF-TOKEN=([^;]+)/);
-    if (match) return decodeURIComponent(match[1]);
     return document.querySelector('meta[name="csrf-token"]')?.getAttribute("content") ?? "";
 }
