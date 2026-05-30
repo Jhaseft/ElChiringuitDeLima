dcdafd439391460a59b4a2a8ad45fc42ea18fda3
Jhaseft <jhasesaat@gmail.com>
2026-05-30 13:38:26 -0400

    Revert "cambio para deploy"
    This reverts commit 2784ffb7e9316090c5494be11c2950ac7e364952.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,11 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-            \App\Http\Middleware\LogRequests::class,
-        ]);
-
-        $middleware->api(prepend: [
-            \App\Http\Middleware\LogRequests::class,
         ]);
 
         // Middlewares para rutas API
